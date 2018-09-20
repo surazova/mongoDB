42a4e526ad3fd1a5f6218781f5ec283bd41e7709
Sitora Urazova <sitoraurazova@gmail.com>
2018-09-20 17:59:58 +0000

    Lesson 7 mongodb

--- test/connection.js
+++ test/connection.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 
-//connect to mongoDB
+//We need to create a hook for mocha (like a condition)
+
+//Connect to the database before the test is run: 
+before(function(done){  //added the done parameter
+    //connect to mongoDB
 mongoose.connect('mongodb://localhost/testaroo', { useNewUrlParser: true }); //testaroo is a database on mongodb
 
 mongoose.connection.once('open', function(){  //.once is an event listener 
     console.log('Connection has been made!');
+    done();
 }).on('err', function(error){
     console.log('Connection error:', error);
 });
+});
+
+//You are telling mocha to wait until the connection has been made, before you run the tests, using the mocha hook 
--- test/saving_test.js
+++ test/saving_test.js
@@ -1,6 +1,11 @@
-//Lesson 6 mongoDB
-//How to create and save data to mongoDB
-//renaed the demo_test.js file to saving_test.js 
+//Lesson 7 mongoDB
+//Using ES6 Library (instead of the D4)
+//This lesson is based on an error 
+
+
+//es6 promises
+//this is not required for c9---> mongoose.Promise = global.Promise 
+
 
 const assert = require('assert');
 const MarioChar = require('../models/mariochar') //we are going to make a new mario character 
